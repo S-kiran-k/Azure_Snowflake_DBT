26ffc26699261c9cb69af0deaf1e778369d8900a
S-kiran-k <skirankumar.cse2021@citchennai.net>
2026-07-06 10:11:06 +0000

    added new models and snapshots

--- airbnb_snowflake_dbt/models/silver/silver_bookings.sql
+++ airbnb_snowflake_dbt/models/silver/silver_bookings.sql
@@ -1,7 +1,8 @@
 {{  
     config(
     materialized='incremental',
-    unique_key='BOOKING_ID'
+    unique_key='BOOKING_ID',
+    on_schema_change='append_new_columns'
     )
 }}
 
@@ -11,6 +12,7 @@ SELECT
     BOOKING_DATE,
     {{multiply("NIGHTS_BOOKED","BOOKING_AMOUNT",2)}} + CLEANING_FEE + SERVICE_FEE AS TOTAL_AMOUNT,
     BOOKING_AMOUNT,
+    BOOKING_STATUS,
     CREATED_AT
 FROM 
     {{ref('bronze_bookings')}}
